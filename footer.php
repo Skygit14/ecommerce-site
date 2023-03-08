460eceaeab7cb726db3547448addfa4ca5fb2f29
Sarvesh Kumar Yadav <120575010+Skygit14@users.noreply.github.com>
2023-03-08 19:02:12 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="contact.php">Contact</a>
+            <a href="shop.php">Shop</a>
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="login.php">Login</a>
+            <a href="register.php">Register</a>
+            <a href="orders.php">My Orders</a>
+            <a href="cart.php">My Cart</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <p> <i class="fas fa-phone"></i> +91 7991597044 </p>
+            <p> <i class="fas fa-phone"></i> +91 9721551185 </p>
+            <p> <i class="fas fa-envelope"></i> sarveshamethi147@gmail.com </p>
+            <p> <i class="fas fa-map-marker-alt"></i> AMETHI - INDIA (227405) </p>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+            <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+            <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+            <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+        </div>
+
+    </div>
+
+    <div class="credit">&copy; Designed by @ <?php echo date('Y'); ?> by <span>Mr. Sarvesh Kumar Yadav</span> </div>
+
+</section>
